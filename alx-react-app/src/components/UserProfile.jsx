65122bd6d67dd39c90466dcf2cc0da86aca2cbd7
indekwe <findekwe@gmail.com>
2024-08-10 15:26:44 -0700

    user profile added in react app

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,10 @@
+function UserProfile(props){
+   return (
+   <>
+    <h2>{props.name}</h2>
+    <p>Age: {props.age}</p>
+    <p>Bio: {props.bio}</p>
+    </>
+    )
+} 
+export default UserProfile
