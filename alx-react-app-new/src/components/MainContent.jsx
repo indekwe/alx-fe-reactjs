08eb781ed876ec25d6017a5696b9b3039ff57433
indekwe <findekwe@gmail.com>
2024-08-19 18:56:09 -0700

    alx-react-app-new added

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent (){
+    return(
+<main style={{backgroundColor:'red', fontWeight:'bolder', fontSize:'20px', color:'white'}}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+    )
+}
+export default MainContent 
