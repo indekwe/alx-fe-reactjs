6a9c7bf931be55f5e1c1b85ded9bacf35c8d87b9
indekwe <findekwe@gmail.com>
2024-08-21 05:33:45 -0700

    updated

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,3 +1,5 @@
 import React from "react"
 import { createContext } from "react"
-export const UserContext=React.createContext()
+
+const UserContext=React.createContext()
+export default UserContext;
