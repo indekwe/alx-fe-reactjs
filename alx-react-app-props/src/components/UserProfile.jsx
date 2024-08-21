e3c579d94bd287efb439c4d605132d967f77ab75
indekwe <findekwe@gmail.com>
2024-08-21 05:40:34 -0700

    updated commit

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,3 +1,6 @@
+import React from "react"
+import { useContext } from "react"
+import UserContext from "./UserContext"
 function UserProfile(props){
    return (
    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
