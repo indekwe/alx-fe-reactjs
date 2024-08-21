e5e9e601a684b3820af7bce7a00929fbca49b7f9
indekwe <findekwe@gmail.com>
2024-08-21 05:20:59 -0700

    updates on alx-react app props

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import {UserContext} from './UserContext' 
+import {UserContext} from './components/UserContext' 
 import ProfilePage from './components/ProfilePage';
 function App() {
   const [count, setCount] = useState(0)
