ab004192286985b1fc890b69d9222e3c3628e7e4
indekwe <findekwe@gmail.com>
2024-08-28 23:43:36 -0700

    update on recipes app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
-import { path } from 'react-router-dom'
 import './App.css'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
