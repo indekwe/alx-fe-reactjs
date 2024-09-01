9cd3845e44b5402ed96fea8eb295ce2e8b8cb03e
indekwe <findekwe@gmail.com>
2024-08-31 17:26:22 -0700

    well handled form added

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import FormikForms from './components/FormikForms'
+function App() {
+  return (
+    <>
+      <FormikForms></FormikForms>
+    </>
+  )
+}
+
+export default App
