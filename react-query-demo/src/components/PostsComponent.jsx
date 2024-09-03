156029043a82b4381042cc8c93a14b785768d90f
indekwe <findekwe@gmail.com>
2024-09-03 03:21:07 -0700

    update on fetching and cachin of data

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -8,12 +8,12 @@ const fetchData = async () => {
 
 const PostsComponent = () => {
     // Use the useQuery hook to handle data fetching and caching
-    const { data, error, isLoading,refetch } = useQuery('fetchData', fetchData);
+    const { data, error, isLoading,refetch, isError } = useQuery('fetchPosts', fetchData);
 
     // Handle loading state
     if (isLoading) return <div>Loading...</div>;
     // Handle error state
-    if (error) return <div>Error loading data</div>;
+    if (isError) return <div>Error loading data</div>;
 
     // Render the fetched data
     return (
