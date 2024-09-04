6076f689acf49669c139ca641e2beff52f4fceb9
indekwe <findekwe@gmail.com>
2024-09-04 16:41:18 -0700

    addition of components with protected routeupdate fith

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,6 +2,7 @@ import Profile from './components/Profile'
 import ProfileDetails from './components/ProfileDetails'
 import ProfileSettings from './components/ProfileSettings'
 import BlogPosts from './components/blogPosts'
+import ProtectedRoute from './components/ProtectedRoute'
 import {BrowserRouter  as Router,Routes,Route} from 'react-router-dom'
 import UserLoggin from './components/UserLoggin'
 function App() {
@@ -12,7 +13,8 @@ function App() {
     <Routes>       
       <Route path='/blog/:id' element={<BlogPosts></BlogPosts>}></Route>
       <Route path='/loggin' element={<UserLoggin></UserLoggin>}></Route>
-      <Route element={<Profile></Profile>}></Route>
+      <Route path='/profile' element={<ProtectedRoute></ProtectedRoute>}></Route>
+
     </Routes>
   </Router>
   )
