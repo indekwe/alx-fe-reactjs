42379e0f3086870936d20c9cf3969d477cb0f487
indekwe <findekwe@gmail.com>
2024-09-10 07:42:57 -0700

    updates

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -1,21 +1,9 @@
 import React from "react";
 import { render, screen, fireEvent } from '@testing-library/react';
 import TodoList from '../components/TodoList'
-import TodoForm from '../components/TodoForm'
 test('render task list', () => {
   render(<TodoList></TodoList>)
   expect(screen.getAllByRole('list').toBeInTheDocument())
 })
-test('task remove', () => {
-  render(<TodoList></TodoList>)
-  const removeTask=jest.fn()
-fireEvent.click(screen.getAllByText('remove'))
-  expect(removeTask).toHaveBeenCalledTimes(1)
-})
-test('task addition', () => {
-  render(<TodoForm></TodoForm>)
-  const addTask=jest.fn()
-fireEvent.click(screen.getAllByText('add task'))
-  expect(addTask).toHaveBeenCalledTimes(1)
-})
+
 
--- react-todo/src/__tests__/removeTodoForm.test.js
+++
@@ -1,6 +0,0 @@
-test('task remove', () => {
-    render(<TodoList></TodoList>)
-    const removeTask=jest.fn()
-  fireEvent.click(screen.getAllByText('remove'))
-    expect(removeTask).toHaveBeenCalledTimes(1)
-  })
