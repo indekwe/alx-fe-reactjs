18b32acafce32ba3a94b5cab2516fdd714795b82
indekwe <findekwe@gmail.com>
2024-09-22 08:01:20 -0700

    update on Homepage

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate,Link } from "react-router-dom";
 import React from 'react'
 
 function HomePage() {
@@ -25,7 +25,7 @@ function HomePage() {
   return (
       <div>
         <div className="bg-fuchsia-200 sm:bg-slate-200 md:bg-amber-500 w-full sm:h-16 h-32 pt-2 text-center ">
-        <h1 className="font-extrabold text-red-950">RICE RECIPES</h1>
+        <h1 className="font-extrabold text-red-950"><Link to='/'>RICE RECIPES</Link></h1>
         <h4 className="text-rose-800">"Swallow neatness"</h4>
         </div>
         <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1  ">
