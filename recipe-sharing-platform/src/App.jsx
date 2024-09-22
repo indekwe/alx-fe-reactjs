96d0679e6ee909f5e28f4f76edf5a6d78ca03b1a
indekwe <findekwe@gmail.com>
2024-09-22 14:24:46 -0700

    form and its validation added

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RecipeDetail from './components/RecipeDetail';
+import { ToastContainer } from 'react-toastify'
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
   return (
@@ -8,7 +10,9 @@ function App() {
       <Routes>
         <Route path="recipe" element={<HomePage />} />
         <Route path="/recipe/:recipeId" element={<RecipeDetail />} />
+        <Route path="/add-recipe" element={<AddRecipeForm />} />
       </Routes>
+      <ToastContainer />
     </Router>
   );
 }
