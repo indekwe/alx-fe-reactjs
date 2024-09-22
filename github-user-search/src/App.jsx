c8454e89af3996d453f2a6c5159c93484bcaedad
indekwe <findekwe@gmail.com>
2024-09-22 01:45:25 -0700

    services updated

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home'
 import About from './components/About'
 import Contacts from './components/Contacts'
+import Services from './services/Services'
 function App() {
   return (
    <Router>
---
+++ github-user-search/src/services/Services.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+    const githubUrl= process.env._REACT_APP_URL
+    const githubKey= process.env._REACT_APP_GITHUB_API_KEY
+    const fetchApiGithub= async ()=>{
+        try {
+            const response= await fetch(githubUrl, {
+                headers: {
+                    Authorization: `Bearer ${githubKey}`
+                }
+            })
+            if(!response.ok){
+                throw new Error(`Error: ${response.status}`)
+            }
+            const data= await response.json()
+            return data
+        }
+        catch {
+            console.error('Error fetching data:', error)
+        }
+    }
+
+export default fetchApiGithub
