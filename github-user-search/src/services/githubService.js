ff881890d05510b4bce392c4716001692e64073c
indekwe <findekwe@gmail.com>
2024-09-27 15:29:04 -0700

    updated thirdly

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 function fetchUserData(username) {
     const githubUrl= process.env.REACT_APP_URL.replace('{username}',username)
     const githubKey= process.env.REACT_APP_GITHUB_API_KEY
-    return axios.get(githubUrl,{
+    return axios.get('https://api.github.com/users/{username}',{
         headers: {
             Authorization: `token ${githubKey}`
         }
